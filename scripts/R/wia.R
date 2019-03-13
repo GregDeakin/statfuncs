bd97defba9e26d91e2a052c16f3ebb5cb83ecb18
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-03-13 10:53:45 +0000

    Update wia.R

--- scripts/R/wia.R
+++ scripts/R/wia.R
@@ -1,4 +1,11 @@
-# Willmot Index of Agreement (d)
+#' Willmot Index of Agreement (d)
+#'
+#' wia calculates the Willmot index of agreement of simulated to observed data.
+#'
+#' @param obs observed data vector 
+#' @param sim simulated data vector
+ 
+
 wia <- function (obs,sim) {
   d <- 1 -  (sum((obs - sim)^2)   / sum( ( abs(sim - mean(obs)) + abs(obs - mean(obs)) )^2 ))
   d <- c(d,mean(abs(obs-sim)/abs(obs)))
