84fa29034163c78274de40e8ec2157d2b8975fed
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-03-15 15:32:12 +0000

    Update varcheck.R

--- scripts/R/varcheck.R
+++ scripts/R/varcheck.R
@@ -8,9 +8,9 @@ varcheck <- function(DT,
   #DT[,(var(value)),by=group][,colMeans(.SD),.SDcols="V1"]
   #exp1[,.(.N*(mean(prop_f1)-mean(DT$value))^2),by=l1][,sum(.SD)/(.N-1),.SDcols="V1"]
   
-  within  <- mean(DT[,(var(value)),by=group][[2]])
+  within  <- mean(DT[,(var(get(value))),by=get(group)][[2]])
   
-  between <- sum(DT[,.(.N*(mean(value)-mean(DT$value))^2),by=group][[2]])/(length(unique(DT$group))-1)
+  between <- sum(DT[,.(.N*(mean(get(value))-mean(DT[[value]]))^2),by=group][[2]])/(length(unique(DT[[group]]))-1)
   
   return(c(within=within,between=between,ratio=within/between))
   
