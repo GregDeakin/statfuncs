43a81a2ada918de7b3e94272d3be616b880ec2f0
Greg Deakin <GregDeakin@users.noreply.github.com>
2019-03-15 16:30:19 +0000

    Update varcheck.R

--- scripts/R/varcheck.R
+++ scripts/R/varcheck.R
@@ -3,6 +3,7 @@ varcheck <- function(DT,
                      value,
                      group ) {
      
+  requireNamespace("data.table", quietly = TRUE)
   DT <- as.data.table(DT)
   #DT[,(var(value)),by=group][,colMeans(.SD),.SDcols="V1"]
   #exp1[,.(.N*(mean(prop_f1)-mean(DT$value))^2),by=l1][,sum(.SD)/(.N-1),.SDcols="V1"]
