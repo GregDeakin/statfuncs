d047e9935243cf44cbd357bf2b87ffa1479b76e4
GregDeakin <greg.deakin@emr.ac.uk>
2019-04-03 09:36:12 +0100

    Added glm.sum and etc.

--- scripts/R/varcheck.R
+++ scripts/R/varcheck.R
@@ -1,4 +1,17 @@
+#' Variance check function
+#' 
+#' Simple function to check within and between group variance
+#' Equivalent to Mean Sq (and F) values from one-way anova
+#' 
+#' @param DT data as data.table (or data frame) object
+#' @value dependent variable column of DT 
+#' @group independent variable (factor) column of DT 
+#' 
 #' @import data.table
+#' 
+#' @example
+#' DT <- data.table(dv=c(rnorm(100,10,2),rnorm(100,20,2)),iv=as.factor(c(rep("A",100),rep("B",100))))
+#' varcheck(DT,"dv","iv")
 varcheck <- function(DT,
                      value,
                      group ) {
